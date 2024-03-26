18b828615b95d37912c5ef4f9ec5138016e008b0
Osomizu <jae.dreijlingg@outlook.com>
2024-03-27 00:35:34 +0100

    tryna do it
    desperately trying to understand yeager

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module waterworld {
     requires hanyaeger;
 
     exports com.github.hanyaeger.tutorial;
+    exports com.github.hanyaeger.tutorial.scenes;
 
     opens audio;
     opens backgrounds;
